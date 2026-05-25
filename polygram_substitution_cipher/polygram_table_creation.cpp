ecb1b9f4fc52116bf8bc70910a7221cd97ddd97e
SadnurIslam <sadnurislam2000@gmail.com>
2026-05-26 01:20:47 +0600

    transposition cipher

--- polygram_substitution_cipher/polygram_table_creation.cpp
+++ polygram_substitution_cipher/polygram_table_creation.cpp
@@ -17,17 +17,24 @@ int main(){
             }
         }
     }
-    int n = vs.size();
-    for(int i=0;i<n;i++){
-        int sz = vv.size();
-        int r = rand()%sz;
-        fout<<vs[i]<<" "<<vv[r]<<endl;
-        vv.erase(vv.begin()+r);
-    }
 
+    // without built in shuffle
+    // for(int i=0;i<n;i++){
+        //     int sz = vv.size();
+        //     int r = rand()%sz;
+        //     fout<<vs[i]<<" "<<vv[r]<<endl;
+        //     vv.erase(vv.begin()+r);
+        // }
+
+    int n = vs.size();
+    shuffle(vv.begin(),vv.end(),default_random_engine(rand()));
+    for(int i=0;i<n;i++){
+        fout<<vs[i]<<" "<<vv[i]<<endl;
+    }
+    
     vs.clear();
     vv.clear();
-
+    
     for(char i='a';i<='z';i++){
         for(int j='a';j<='z';j++){
             string s="";
@@ -37,31 +44,27 @@ int main(){
             vv.push_back(s);
         }
     }
-
+    
     n = vs.size();
+    shuffle(vv.begin(),vv.end(),default_random_engine(rand()));
     for(int i=0;i<n;i++){
-        int sz = vv.size();
-        int r = rand()%sz;
-        fout<<vs[i]<<" "<<vv[r]<<endl;
-        vv.erase(vv.begin()+r);
+        fout<<vs[i]<<" "<<vv[i]<<endl;
     }
-
+    
     vs.clear();
     vv.clear();
-
+    
     for(char i='a';i<='z';i++){
         string s="";
         s.push_back(i);
         vs.push_back(s);
         vv.push_back(s);
     }
-
+    
     n = vs.size();
+    shuffle(vv.begin(),vv.end(),default_random_engine(rand()));
     for(int i=0;i<n;i++){
-        int sz = vv.size();
-        int r = rand()%sz;
-        fout<<vs[i]<<" "<<vv[r]<<endl;
-        vv.erase(vv.begin()+r);
+        fout<<vs[i]<<" "<<vv[i]<<endl;
     }
     
 }
---
+++ transposition_cipher.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string encrypt(string msg, int col){
+    int n = msg.size();
+    int row = ceil(1.0*n/col);
+    vector<vector<char>>table(row,vector<char>(col,'?'));
+    int k = 0;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col && k<n;j++){
+            table[i][j] = msg[k];
+            k++;
+        }
+    }
+    string cipher = "";
+    for(int i=0;i<col;i++){
+        for(int j=0;j<row;j++){
+            if(table[j][i]=='?')continue;
+            cipher+=table[j][i];
+        }
+    }
+    return cipher;
+}
+
+string decrypt(string cipher, int col){
+    string plain = "";
+    int n = cipher.size();
+    int row = n/col;
+    int rem = n-row*col;
+    vector<vector<char>>table(row+1,vector<char>(col,'?'));
+    int k = 0;
+    for(int i=0;i<col;i++){
+        for(int j=0;j<row && k<n;j++){
+            table[j][i] = cipher[k];
+            k++;
+        }
+        if(rem>0 && k<n){
+            table[row][i] = cipher[k];
+            rem--;
+            k++;
+        }
+    }
+    for(int i=0;i<=row;i++){
+        for(int j=0;j<col;j++){
+            if(table[i][j]=='?')break;
+            plain+=table[i][j];
+        }
+    }
+    return plain;
+}
+
+int main(){
+    
+    string msg = "DEPARTMENTOFCOMPUTERSCIENCEANDENGINEERING";
+    int width;
+    cout<<"Width of transposition table: ";
+    cin>>width;
+    cout<<"Plain text: "<<msg<<endl;
+    string cipher = encrypt(msg,width);
+    cout<<"Cipher text: "<<cipher<<endl;
+    
+    string plain = decrypt(cipher,width);
+    cout<<"Plain text: "<<plain<<endl;
+    cout<<((msg==plain)?"success":"failed")<<endl;
+}
